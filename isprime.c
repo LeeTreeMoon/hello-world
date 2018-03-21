455fe5084ab996bf8109eb3a95f8d5dca744e1e9
LeeTreeMoon <37114694+LeeTreeMoon@users.noreply.github.com>
2018-03-21 23:46:21 +0800

    prime.c
    super prime

--- isprime.c
+++ isprime.c
@@ -1,6 +1,9 @@
 /*满足下列条件的自然数称为超级素数:该数本身,所有数字之和,所有数字之积以及所有数字的平方和都是素数.例如113就是一个超级素数.
 求[100,9999]之内:
 (1)超级素数的个数.(2)所有超级素数之和.(3)最大的超级素数.*/
+/*满足下列条件的自然数称为超级素数:该数本身,所有数字之和,所有数字之积以及所有数字的平方和都是素数.例如113就是一个超级素数.
+求[100,9999]之内:
+(1)超级素数的个数.(2)所有超级素数之和.(3)最大的超级素数.*/
 #include<stdio.h> 
 
 int a,b,c,d,count,sum,multi,square;
@@ -23,7 +26,10 @@ int sum_bit(int num)
 int multi_bit(int num) 
 {
 	devide(num);
+	if(d!=0)
 	multi=a*b*c*d;
+	else 
+	multi=a*b*c;
     return multi;
 }//积 
 
@@ -37,15 +43,20 @@ int square_sum_bit(int num)
 bool isprime(int num) 
 {
 	int i;
-	for(i=2;i<num;i++)
+	if(num<2) return false;
+	else if(num==2) return true;
+	else
 	{
-		if(num%i==0)
-        {
-        	return false;
-        	break;
-	    }
+		for(i=2;i<num;i++)
+    	{
+    		if(num%i==0)
+            {
+            	return false;
+            	break;
+	        }
+    	}
+    	return true;
 	}
-	return true;
 }//素数 
 
 int main() 
