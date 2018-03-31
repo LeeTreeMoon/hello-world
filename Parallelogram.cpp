cc2ab278be45773768ea22fd76aea9a2c40b7dfe
LeeTreeMoon <37114694+LeeTreeMoon@users.noreply.github.com>
2018-03-31 16:54:06 +0800

    Parallelogram.cpp
    遗留问题：显示平行四边形

---
+++ Parallelogram.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+class Parallelogram 
+{
+	public:
+		Parallelogram(){angle=1.047;lena=3;lenb=2.9;}
+//		~Parallelogram();
+		void show();
+		void area();
+		void height();
+		void length();
+	private:
+		float angle,lena,lenb;
+};
+
+void Parallelogram::area()
+{
+	cout<<lena*lenb*sin(angle)<<endl;
+}
+
+void Parallelogram::height()
+{
+	cout<<lena*sin(angle)<<"或者"<<lenb*sin(angle)<<endl;
+}
+
+void Parallelogram::length()
+{
+	cout<<2*(lena+lenb)<<endl;
+}
+
+void Parallelogram::show()
+{
+	int i,j,k;
+	for(i=0;i<round(lenb);i++)
+	{
+		for(k=0;k<i;k++)
+		cout<<' ';
+		for(j=0;j<round(lena);j++)
+		cout<<'*'<<' ';
+		cout<<endl;
+	}
+}
+
+int main() 
+{
+  Parallelogram pg;
+  pg.show();
+  pg.area();
+  pg.height();
+  pg.length();
+  return 0;
+}
