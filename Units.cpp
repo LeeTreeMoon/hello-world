20de93fcf69cf06dcd2d11f3e4f9a9826fa93728
LeeTreeMoon <37114694+LeeTreeMoon@users.noreply.github.com>
2018-06-13 00:36:42 +0800

    Update Units.cpp

--- Units.cpp
+++ Units.cpp
@@ -1,4 +1,11 @@
+//单位转换程序，输入值和原单位，新单位，返回新单位下的值。
+//重量单位：Kg, g, lb, gr,......
+//压力单位：Pa, MPa, psi,......
+//长度单位：m, km, ft, in,......
+//其它单位：md, cp,......
+//还有很多单位转换，有些单位是其它单位组合出来的
 #include<iostream> 
+#include<vector>
 using namespace std;
 
 class Unit
@@ -12,84 +19,111 @@ class Base:public Unit
 {
 	private:
 		int mark,now,next;
-		string lenth[9];
-		double len_mul[9];
-		string time[5];
-		double time_mul[5];
-		string quality[9];
-		double qual_mul[9];
+		vector<string> length_name;
+		vector<double> length_mul;
+		vector<string> time_name;
+		vector<double> time_mul;
+		vector<string> quality_name;
+		vector<double> quality_mul;
+		vector<string> temperature_name;
+		vector<double> temperature_mul;
+		
 	public:
 		Base()
 		{
 			mark=0;
 			now=next=100;
-			lenth[0]="in"; lenth[1]="ft"; lenth[2]="yd"; lenth[3]="sm";
-			lenth[4]="mm"; lenth[5]="cm"; lenth[6]="km"; lenth[7]="m";
-			lenth[8]="nm"; 
-			len_mul[0]=0.0254; len_mul[1]=0.3048; len_mul[2]=0.9144; len_mul[3]=1852;
-			len_mul[4]=0.001;   len_mul[5]=0.1;    len_mul[6]=1000;   len_mul[7]=1;
-			len_mul[8]=0.000000001;
+			length_name.push_back("in"); length_name.push_back("ft"); length_name.push_back("yd"); length_name.push_back("sm");
+			length_name.push_back("mm"); length_name.push_back("cm"); length_name.push_back("km"); length_name.push_back("m");
+			length_name.push_back("nm"); 
 			
-			time[0]="ms"; time[1]="s"; time[2]="min"; time[3]="h"; time[4]="d";
-			time_mul[0]=0.001; time_mul[1]=1; time_mul[2]=60; time_mul[3]=3600;
-			time_mul[4]=216000;
+			length_mul.push_back(0.0254); length_mul.push_back(0.3048); length_mul.push_back(0.9144); length_mul.push_back(1852);
+			length_mul.push_back(0.001); length_mul.push_back(0.01);   length_mul.push_back(1000);   length_mul.push_back(1);
+			length_mul.push_back(0.000000001);
 			
-			quality[0]="g"; quality[1]="t"; quality[2]="dwt"; quality[3]="cwt";
-			quality[4]="oz"; quality[5]="kg"; quality[6]="mg"; quality[7]="lb";
-			quality[8]="dr";
-			qual_mul[0]=1; qual_mul[1]=0.000001; qual_mul[2]=1.5551738; qual_mul[3]=50802.3454400;
-			qual_mul[4]=28.3495231; qual_mul[5]=1000; qual_mul[6]=0.001; qual_mul[7]=453.59237;
-			qual_mul[8]=1.7718452;
+			time_name.push_back("ms"); time_name.push_back("s"); time_name.push_back("min"); time_name.push_back("h"); time_name.push_back("d");
+			time_mul.push_back(1.0/60); time_mul.push_back(1); time_mul.push_back(60); time_mul.push_back(3600);time_mul.push_back(216000) ;
+			
+			quality_name.push_back("g"); quality_name.push_back("t"); quality_name.push_back("dwt"); quality_name.push_back("cwt");
+			quality_name.push_back("oz"); quality_name.push_back("kg"); quality_name.push_back("mg"); quality_name.push_back("lb");
+			quality_name.push_back("dr");
+			
+			quality_mul.push_back(1); quality_mul.push_back(0.000001); quality_mul.push_back(1.5551738); quality_mul.push_back(50802.3454400);
+			quality_mul.push_back(28.3495231); quality_mul.push_back(0.001); quality_mul.push_back(1000); quality_mul.push_back(453.59237);
+			quality_mul.push_back(1.7718452);
+			
+			temperature_name.push_back("摄氏度(℃)"); temperature_name.push_back("华氏度(℉)"); temperature_name.push_back("开氏度(K)");
+            temperature_name.push_back("列氏度(°Re)"); temperature_name.push_back("兰氏度(°R)");
+
+            temperature_mul.push_back(1); temperature_mul.push_back(33.8); temperature_mul.push_back(274.15);
+            temperature_mul.push_back(0.8); temperature_mul.push_back(493.47);			
 		}
 		virtual bool find(string str1,string str2)
 		{
-			cout<<sizeof(lenth)<<endl;
-			cout<<sizeof(len_mul)<<endl;
-			cout<<sizeof(time)<<endl;
-			cout<<sizeof(time_mul)<<endl;
-			for(int i=0;i<9;i++)
+			vector<string>::iterator it1,it2;
+			for(it1=length_name.begin();it1!=length_name.end();it1++)
 			{
-				if(str1==lenth[i])
+				if(str1==*it1)
 				{
-					for(int j=0;j<9;j++)
+					for(it2=length_name.begin();it2!=length_name.end();it2++)
 					{
-						if(str2==lenth[j])
+						if(str2==*it2)
 						{
 							mark=1;
-							now=i;
-							next=j;
+							now=it1-length_name.begin();
+							next=it2-length_name.begin();
+//							cout<<now<<" " <<next<<endl;
 							return true;
 						}
 					}
 				}
 			}
-			for(int i=0;i<5;i++)
+			for(it1=time_name.begin();it1!=time_name.end();it1++)
 			{
-				if(str1==time[i])
+				if(str1==*it1)
 				{
-					for(int j=0;j<5;j++)
+					for(it2=time_name.begin();it2!=time_name.end();it2++)
 					{
-						if(str2==time[j])
+						if(str2==*it2)
 						{
-							mark=1;
-							now=i;
-							next=j;
+							mark=2;
+							now=it1-time_name.begin();
+							next=it2-time_name.begin();
+//							cout<<now<<" " <<next<<endl;
 							return true;
 						}
 					}
 				}
 			}
-			for(int i=0;i<9;i++)
+			for(it1=quality_name.begin();it1!=quality_name.end();it1++)
 			{
-				if(str1==quality[i])
+				if(str1==*it1)
 				{
-					for(int j=0;j<9;j++)
+					for(it2=quality_name.begin();it2!=quality_name.end();it2++)
 					{
-						if(str2==quality[j])
+						if(str2==*it2)
 						{
-							mark=1;
-							now=i;
-							next=j;
+							mark=3;
+							now=it1-quality_name.begin();
+							next=it2-quality_name.begin();
+//							cout<<now<<" " <<next<<endl;
+							return true;
+						}
+					}
+				}
+			}
+			for(it1=temperature_name.begin();it1!=temperature_name.end();it1++)
+			{
+				if(str1==*it1)
+				{
+					for(it2=temperature_name.begin();it2!=temperature_name.end();it2++)
+					{
+						if(str2==*it2)
+						{
+							mark=4;
+							now=it1-temperature_name.begin();
+							next=it2-temperature_name.begin();
+//							cout<<now<<" " <<next<<endl;
 							return true;
 						}
 					}
@@ -97,6 +131,7 @@ class Base:public Unit
 			}
 			if(!mark) return false;
 		}
+		
 		virtual void trans(double num)
 		{
 			if(now==next)
@@ -104,11 +139,13 @@ class Base:public Unit
 				cout<<num;
 				return;
 			}
+			vector<double>::iterator it1,it2;
 			switch(mark)
 			{
-				case 1:cout<<num*len_mul[now]/len_mul[next]; break;
-				case 2:cout<<num*time_mul[now]/time_mul[next]; break;
-				case 3:cout<<num*qual_mul[now]/qual_mul[next]; break;
+				case 1:it1=now+length_mul.begin();it2=next+length_mul.begin();cout<<"换算结果："<<num*(*it1)/(*it2)<<endl<<endl; break;
+				case 2:it1=now+time_mul.begin();it2=next+time_mul.begin();cout<<"换算结果："<<num*(*it1)/(*it2)<<endl<<endl; break;
+				case 3:it1=now+quality_mul.begin();it2=next+quality_mul.begin();cout<<"换算结果："<<num*(*it1)/(*it2)<<endl<<endl; break;
+				case 4:it1=now+temperature_mul.begin();it2=next+temperature_mul.begin();cout<<"换算结果："<<num*(*it2)/(*it1)<<endl<<endl; break;
 			}
 		}
 };
@@ -117,34 +154,157 @@ class Mix:public Unit
 {
 	private:
 		int mark,now,next;
-	    string area[10];
-	    double area_mul[10];
-	    
+	    vector<string> area_name;
+	    vector<double> area_mul;
+	    vector<string> power_name;
+	    vector<double> power_mul;
+	    vector<string> pressure_name;
+	    vector<double> pressure_mul;
+	    vector<string> volume_name;
+	    vector<double> volume_mul;
+	    vector<string> velocity_name;
+	    vector<double> velocity_mul;
 	public:
 		Mix()
 		{
 			mark=0;
 			now=next=100;
-			area[0]="ha"; area[1]="m2"; area[2]="cm2"; area[3]="sq_mi";
-			area[4]="sq_rd"; area[5]="sq_ft"; area[6]="sq in"; area[7]="sq_yd" ;
-			area[8]="mm2"; area[9]="km2";
-			area_mul[0]=10000; area_mul[1]=1; area_mul[2]=0.0001; area_mul[3]=2589988.1;
-			area_mul[4]=25.292853; area_mul[5]=0.09290304; area_mul[6]=0.00064516; area_mul[7]=0.83612736;
-			area_mul[8]=0.000001; area_mul[9]=1000000;
+			area_name.push_back("ha"); area_name.push_back("m2"); area_name.push_back("cm2"); area_name.push_back("sq_mi");
+			area_name.push_back("sq_rd"); area_name.push_back("sq_ft"); area_name.push_back("sq_in"); area_name.push_back("sq_yd");
+			area_name.push_back("mm2"); area_name.push_back("km2");
+			
+			area_mul.push_back(10000); area_mul.push_back(1); area_mul.push_back(0.0001); area_mul.push_back(2589988.1);
+			area_mul.push_back(25.292853); area_mul.push_back(0.09290304); area_mul.push_back(0.00064516); area_mul.push_back(0.83612736);
+			area_mul.push_back(1000000) ; area_mul.push_back(0.000001);
+			
+			power_name.push_back("w"); power_name.push_back("J/s"); power_name.push_back("N.m/s"); 
+            power_name.push_back("ps"); power_name.push_back("kcal/s"); power_name.push_back("kw"); 
+            power_name.push_back("hp"); power_name.push_back("ft·lb/s"); power_name.push_back("Btu/s"); 
+            power_name.push_back("kg.m/s");
+
+            power_mul.push_back(1); power_mul.push_back(1); power_mul.push_back(1); 
+            power_mul.push_back(0.0013596); power_mul.push_back(0.000239); power_mul.push_back(0.001); 
+            power_mul.push_back(0.001341); power_mul.push_back(0.7375621); power_mul.push_back(0.0009478); 
+            power_mul.push_back(0.1019716);
+            
+            pressure_name.push_back("pa"); pressure_name.push_back("hpa"); pressure_name.push_back("mbar"); 
+            pressure_name.push_back("kpa"); pressure_name.push_back("bar"); pressure_name.push_back("MPa"); 
+            pressure_name.push_back("mmHg"); pressure_name.push_back("inHg"); pressure_name.push_back("psi"); 
+            pressure_name.push_back("atm"); pressure_name.push_back("psf"); pressure_name.push_back("kgf/cm2"); 
+            pressure_name.push_back("kgf/m2"); 
+
+            pressure_mul.push_back(1); pressure_mul.push_back(0.01); pressure_mul.push_back(0.01); 
+            pressure_mul.push_back(0.001); pressure_mul.push_back(0.00001); 
+            pressure_mul.push_back(0.000001); pressure_mul.push_back(0.0075006); 
+            pressure_mul.push_back(0.0002953); pressure_mul.push_back(0.000145); pressure_mul.push_back(0.0000098692); 
+            pressure_mul.push_back(0.0208854); pressure_mul.push_back(0.0000102); pressure_mul.push_back(0.1019716); 
+            
+            volume_name.push_back("m3"); volume_name.push_back("l"); volume_name.push_back("ml"); 
+            volume_name.push_back("cm3"); volume_name.push_back("dm3"); volume_name.push_back("dl"); 
+            volume_name.push_back("cl"); volume_name.push_back("hl"); volume_name.push_back("ul"); 
+            volume_name.push_back("mm3"); volume_name.push_back("cu_in"); volume_name.push_back("cu_yd"); 
+            volume_name.push_back("cu_ft"); volume_name.push_back("uk_gal"); volume_name.push_back("us_gal"); 
+            volume_name.push_back("uk_loz"); volume_name.push_back("us_loz"); 
+
+            volume_mul.push_back(1); volume_mul.push_back(1000); volume_mul.push_back(1000000); 
+            volume_mul.push_back(1000000); volume_mul.push_back(1000); volume_mul.push_back(10000); 
+            volume_mul.push_back(100000); volume_mul.push_back(10); volume_mul.push_back(1000000000); 
+            volume_mul.push_back(1000000000); volume_mul.push_back(61023.8445022); volume_mul.push_back(1.3079528); 
+            volume_mul.push_back(35.3147248); volume_mul.push_back(219.9691573); volume_mul.push_back(264.1720524); 
+            volume_mul.push_back(35198.873636); volume_mul.push_back(34164.6737274); 
+            
+            velocity_name.push_back("m/s"); velocity_name.push_back("km/h"); velocity_name.push_back("km/s");
+            velocity_name.push_back("mach"); velocity_name.push_back("in/s"); velocity_name.push_back("c"); 
+            velocity_name.push_back("mile/h"); 
+
+            velocity_mul.push_back(1); velocity_mul.push_back(3.6); velocity_mul.push_back(0.001); 
+            velocity_mul.push_back(0.0029386); velocity_mul.push_back(39.370079); velocity_mul.push_back(0.0000000033356); 
+            velocity_mul.push_back(2.236936); 
 		}
+		
 		virtual bool find(string str1,string str2)
 		{
-			for(int i=0;i<10;i++)
+			vector<string>::iterator it1,it2;
+			for(it1=area_name.begin();it1!=area_name.end();it1++)
 			{
-				if(area[i]==str1)
+				if(str1==*it1)
 				{
-					for(int j=0;j<10;j++)
+					for(it2=area_name.begin();it2!=area_name.end();it2++)
 					{
-						if(area[j]==str2)
+						if(str2==*it2)
 						{
 							mark=1;
-							now=i;
-							next=j;
+							now=it1-area_name.begin();
+							next=it2-area_name.begin();
+//							cout<<now<<" " <<next<<endl;
+							return true;
+						}
+					}
+				}
+			}
+			for(it1=power_name.begin();it1!=power_name.end();it1++)
+			{
+				if(str1==*it1)
+				{
+					for(it2=power_name.begin();it2!=power_name.end();it2++)
+					{
+						if(str2==*it2)
+						{
+							mark=2;
+							now=it1-power_name.begin();
+							next=it2-power_name.begin();
+//							cout<<now<<" " <<next<<endl;
+							return true;
+						}
+					}
+				}
+			}
+			for(it1=pressure_name.begin();it1!=pressure_name.end();it1++)
+			{
+				if(str1==*it1)
+				{
+					for(it2=pressure_name.begin();it2!=pressure_name.end();it2++)
+					{
+						if(str2==*it2)
+						{
+							mark=3;
+							now=it1-pressure_name.begin();
+							next=it2-pressure_name.begin();
+//							cout<<now<<" " <<next<<endl;
+							return true;
+						}
+					}
+				}
+			}
+			for(it1=volume_name.begin();it1!=volume_name.end();it1++)
+			{
+				if(str1==*it1)
+				{
+					for(it2=volume_name.begin();it2!=volume_name.end();it2++)
+					{
+						if(str2==*it2)
+						{
+							mark=4;
+							now=it1-volume_name.begin();
+							next=it2-volume_name.begin();
+//							cout<<now<<" " <<next<<endl;
+							return true;
+						}
+					}
+				}
+			}
+			for(it1=velocity_name.begin();it1!=velocity_name.end();it1++)
+			{
+				if(str1==*it1)
+				{
+					for(it2=velocity_name.begin();it2!=velocity_name.end();it2++)
+					{
+						if(str2==*it2)
+						{
+							mark=5;
+							now=it1-velocity_name.begin();
+							next=it2-velocity_name.begin();
+//							cout<<now<<" " <<next<<endl;
 							return true;
 						}
 					}
@@ -159,9 +319,14 @@ class Mix:public Unit
 				cout<<num;
 				return;
 			}
+			vector<double>::iterator it1,it2;
 			switch(mark)
 			{
-				case 1:cout<<num*area_mul[now]/area_mul[next]; break;
+				case 1:it1=now+area_mul.begin();it2=next+area_mul.begin();cout<<"换算结果："<<num*(*it1)/(*it2)<<endl<<endl; break;
+				case 2:it1=now+power_mul.begin();it2=next+power_mul.begin();cout<<"换算结果："<<num*(*it2)/(*it1)<<endl<<endl; break;
+				case 3:it1=now+pressure_mul.begin();it2=next+pressure_mul.begin();cout<<"换算结果："<<num*(*it2)/(*it1)<<endl<<endl; break;
+				case 4:it1=now+volume_mul.begin();it2=next+volume_mul.begin();cout<<"换算结果："<<num*(*it2)/(*it1)<<endl<<endl; break;
+				case 5:it1=now+velocity_mul.begin();it2=next+velocity_mul.begin();cout<<"换算结果："<<num*(*it2)/(*it1)<<endl<<endl; break;
 			}
 		}
 };
@@ -171,7 +336,7 @@ int main()
 	bool win;
 	double num;
 	string origin,end;
-	cout<<"请输入正确的数字与单位，所有单位用相应的英文缩写表示，单位内部不允许有空格与括号，若需空格请用下划线连接"<<endl; 
+	cout<<"请输入正确的数字与单位，所有单位用相应的英文缩写表示，单位内部不允许有空格，若需空格请用下划线连接"<<endl; 
 	while(cin>>num>>origin>>end)
 	{
     	Base obj1;
@@ -181,7 +346,7 @@ int main()
 	    else
 	    {
 		    Mix obj2;
-		    obj2.find(origin,end);
+		    win=obj2.find(origin,end);
 		    if(!win) cout<<"Error";
 		    else obj2.trans(num);
 	    }
